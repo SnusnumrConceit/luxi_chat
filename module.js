6389e1f9e85c400ae7dbedbbb7810ae7e33f86b8
luxicat <jkramorevx@gmail.com>
2018-05-17 21:46:29 +0300

    Вынесение опции module в отдельный файл

---
+++ module.js
@@ -0,0 +1,27 @@
+const env = process.env.NODE_ENV;
+
+const rules = {
+  rules: [
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.html$/,
+      use: 'html-loader'
+    },
+    {
+      test: /\.(png|jpg|jpeg|gif)$/,
+      loader: 'file-loader',
+      options: {
+        name (file) {
+          if (env === 'development') {
+            return '[path][name].[ext]'
+          }
+          return '[hash].[ext]'
+        }
+      }
+    }
+  ],
+};
+module.exports = rules;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 "use strict";
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const env = process.env.NODE_ENV;
+const rules = require('./module.js');
 
 const config = {
   entry: {
@@ -12,30 +11,7 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname + '/dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name (file) {
-            if (env === 'development') {
-              return '[path][name].[ext]'
-            }
-            return '[hash].[ext]'
-          }
-        }
-      }
-    ],
-  },
+  module: rules,
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
