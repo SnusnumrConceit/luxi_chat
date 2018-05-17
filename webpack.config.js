35e978afae8b2dd4a9c5e46ca0f9fab5349cf1a6
luxicat <jkramorevx@gmail.com>
2018-05-17 20:48:27 +0300

    Добавление html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
+const config = {
+  entry: {
+    general: './src/index.js'
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    path: path.resolve(__dirname + '/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ]
 };
+
+module.exports = config;
