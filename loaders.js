abf48b040dcff4b0088457fe89edf49108be1fc4
luxicat <jkramorevx@gmail.com>
2018-05-18 21:36:56 +0300

    Добавление babel-loader в конфиг #3

--- loaders.js
+++ loaders.js
@@ -15,6 +15,16 @@ const rules = {
       }]
     },
     {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    {
       test: /\.html$/,
       use: 'html-loader'
     },
