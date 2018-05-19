ee29e5c767c232b03835388e9175e3fb61859515
luxicat <jkramorevx@gmail.com>
2018-05-19 16:03:01 +0300

    Update some play with hot reload. Close #4

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    biba i'm first component
-    chikki-brikki
-    test replies
+    biba i'm an App
+    <first-component></first-component>
   </div>
 </template>
 <script>
+  import firstComponent from './first_component.vue'
   export default {
-    name: 'app'
+    name: 'app',
+    components: {
+      'first-component': firstComponent
+    }
   }
 </script>
+<style lang="sass">
+  body
+    background: red
+
+</style>
---
+++ src/first_component.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    hello I'm first component
+    yeah
+  </div>
+</template>
