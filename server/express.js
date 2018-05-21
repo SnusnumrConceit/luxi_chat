53643504d13fcc2d34f5686d3c297a34b9ab36f3
luxicat <jkramorevx@gmail.com>
2018-05-21 22:22:29 +0300

    Some feature to express.js
    - send file index.html
    - get dist path
    - set directory for static files
    - path module
    - add socket.io

--- server/express.js
+++ server/express.js
@@ -1,6 +1,15 @@
-var express = require('express');
-var app = express();
+"use strict"
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const path = require('path')
+const dist_path = path.resolve(__dirname, '../dist/')
+server.listen(8000)
+
+app.use(express.static('dist'))
 
 app.get('/', function(req, res) {
-  res.send('hello biba');
-});
+  res.sendFile(dist_path + '/index.html')
+})
